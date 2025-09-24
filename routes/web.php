a689c41a124d5c60009bd533461f1f16729c9340
Stifend <stifend@pcr.ac.id>
2025-09-24 09:50:51 +0700

    Penambahan Home

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 
-
+Route::get('/home', [HomeController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
 // Route::get(: '/pcr', function () {
 //     return 'Selamat Datang di Website Kampus PCR!';
 // });
-Route::get( '/mahasiswa/{param1}',[MahasiswaController::class, 'show']);
+Route::get( '//{param1}',[MahasiswaController::class, 'show']);
 
 
 Route::get('/nama/{param1}/nim', function ($param1, $nim) {
@@ -22,3 +23,4 @@ Route::get('/nim/{param1?}', function ($faham = '') {
 Route::get( '/about',function () {
     return view ('halaman-about');
 });
+
